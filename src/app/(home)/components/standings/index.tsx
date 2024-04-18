d0e64ec0a383cdc7a57e87ea0bee3c67f657691a
Mateus Vallim <mateus.vallim@usp.br>
2024-04-18 12:52:35 -0300

    fix: Refactor Standings component to use async/await and separate functions for fetching data

--- src/app/(home)/components/standings/index.tsx
+++ src/app/(home)/components/standings/index.tsx
@@ -1,12 +1,27 @@
+"use client"
 import axios from "axios"
+import { useState, useCallback, useEffect } from "react"
 
 import { handleUpdateChecker } from "@/lib/utils"
 
 import { StandingsList } from "./standings-list"
 
-export async function Standings() {
-	const response = await axios.get("/api/standings-driver-points")
-	const standings: DriverPoints[] = response.data.data
+export function Standings() {
+	const [standings, setStandings] = useState<DriverPoints[]>([])
+
+	const getStandings = useCallback(async () => {
+		await axios
+			.get("/api/standings-driver-points")
+			.then(res => {
+				const data: DriverPoints[] = res.data.data
+				setStandings(data)
+			})
+			.catch(err => console.error(err))
+	}, [])
+
+	useEffect(() => {
+		getStandings()
+	}, [getStandings])
 
 	handleUpdateChecker()
 
