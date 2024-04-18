2337ea02e89538ceb62b705bb79b67d39d628c02
Mateus Vallim <mateus.vallim@usp.br>
2024-04-18 12:33:27 -0300

    fix: Refactor API URLs in standings and constructors components to use async/await and separate functions for fetching data

--- src/app/(home)/components/standings/index.tsx
+++ src/app/(home)/components/standings/index.tsx
@@ -3,19 +3,24 @@ import { handleUpdateChecker } from "@/lib/utils"
 import { StandingsList } from "./standings-list"
 
 export async function Standings() {
-	const response = await fetch(
-		(process.env.NODE_ENV === "development"
-			? "http://localhost:3000"
-			: "https://" + process.env.NEXT_PUBLIC_VERCEL_URL) +
-			"/api/standings-driver-points",
-		{
-			next: {
-				tags: ["get-standings-driver-points"],
-			},
-		}
-	)
-	const data = await response.json()
-	const standings: DriverPoints[] = data.data
+	async function standingsData() {
+		const response = await fetch(
+			(process.env.NODE_ENV === "development"
+				? "http://localhost:3000"
+				: "https://" + process.env.NEXT_PUBLIC_VERCEL_URL) +
+				"/api/standings-driver-points",
+			{
+				next: {
+					tags: ["get-standings-driver-points"],
+				},
+			}
+		)
+		const data = await response.json()
+		const standings: DriverPoints[] = data.data
+		return standings
+	}
+
+	const standings: DriverPoints[] = await standingsData()
 
 	handleUpdateChecker()
 
--- src/app/constructors/components/standings/index.tsx
+++ src/app/constructors/components/standings/index.tsx
@@ -3,35 +3,47 @@ import { handleUpdateChecker } from "@/lib/utils"
 import { StandingsList } from "./standings-list"
 
 export async function Constructors() {
-	const response = await fetch(
-		(process.env.NODE_ENV === "development"
-			? "http://localhost:3000"
-			: "https://" + process.env.NEXT_PUBLIC_VERCEL_URL) +
-			"/api/standings-constructor-points",
-		{
-			next: {
-				tags: ["get-standings-constructor-points"],
-			},
-		}
-	)
+	async function standingsConstructorsData() {
+		const response = await fetch(
+			(process.env.NODE_ENV === "development"
+				? "http://localhost:3000"
+				: "https://" + process.env.NEXT_PUBLIC_VERCEL_URL) +
+				"/api/standings-constructor-points",
+			{
+				next: {
+					tags: ["get-standings-constructor-points"],
+				},
+			}
+		)
 
-	const data = await response.json()
-	const standingsConstructors: ConstructorPoints[] = data.data
+		const data = await response.json()
+		const standingsConstructors: ConstructorPoints[] = data.data
+		return standingsConstructors
+	}
+
+	async function driversData() {
+		const responseDrivers = await fetch(
+			(process.env.NODE_ENV === "development"
+				? "http://localhost:3000"
+				: "https://" + process.env.NEXT_PUBLIC_VERCEL_URL) +
+				"/api/drivers",
+			{
+				next: {
+					tags: ["get-all-current-drivers"],
+				},
+			}
+		)
+		const dataDrivers = await responseDrivers.json()
+		const drivers: Driver[] = dataDrivers.data
+		return drivers
+	}
+
+	const standingsConstructors: ConstructorPoints[] =
+		await standingsConstructorsData()
 
 	handleUpdateChecker()
 
-	const responseDrivers = await fetch(
-		(process.env.NODE_ENV === "development"
-			? "http://localhost:3000"
-			: "https://" + process.env.NEXT_PUBLIC_VERCEL_URL) + "/api/drivers",
-		{
-			next: {
-				tags: ["get-all-current-drivers"],
-			},
-		}
-	)
-	const dataDrivers = await responseDrivers.json()
-	const drivers: Driver[] = dataDrivers.data
+	const drivers: Driver[] = await driversData()
 
 	return (
 		<div className="flex w-full">
